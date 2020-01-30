2058d6ee8ff5334afc17eb9b1897dbf0a8a04606
phongvu2010 <phongvu2010@gmail.com>
2020-01-31 02:06:09 +0700

    New Code

---
+++ src/main/java/ga/hunterdo/barcodeparking/controller/TestController.java
@@ -0,0 +1,20 @@
+package ga.hunterdo.barcodeparking.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/key")
+public class TestController {
+	@Value("${config.server.student-key}")
+	private String key;
+
+	@GetMapping
+	public String fetchProperty() {
+		return key;
+	}
+}
